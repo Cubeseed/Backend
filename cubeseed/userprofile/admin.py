9caafd6d494fe547b130e0e76426730e87468513
Sebastian <sgarciahelguera@github.com>
2023-06-14 16:38:53 -0600

    Add userprofile and userprofilephoto to admin

--- cubeseed/userprofile/admin.py
+++ cubeseed/userprofile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from cubeseed.userprofile.models import UserProfile, UserProfilePhoto
+
+admin.site.register(UserProfile)
+admin.site.register(UserProfilePhoto)
