4fff631769e81e30048428cb96b7c3c555b527db
Akintola Rahmat <akintolarahmah@gmail.com>
2023-07-15 00:51:29 +0000

    fix pylint errors

--- cubeseed/userauth/tests.py
+++ cubeseed/userauth/tests.py
@@ -1,8 +1,10 @@
 from rest_framework.reverse import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import Permission
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class UserAuthAPITest(APITestCase):
     """
