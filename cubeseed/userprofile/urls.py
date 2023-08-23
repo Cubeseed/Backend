3eac2467c828ba8cfdc54d6b1961c7b981869d70
Akintola Rahmat <akintolarahmah@gmail.com>
2023-08-23 13:48:38 +0000

    update user profile url

--- cubeseed/userprofile/urls.py
+++ cubeseed/userprofile/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
 from cubeseed.userprofile import views
+from cubeseed.farm import views 
 
 
 def register_routes(router):
     router.register(r"userprofile", views.UserProfileViewSet)
     router.register(r"userprofilephoto", views.UserProfilePhotoViewSet)
+    router.register(r"farmprofile", views.FarmViewSet)
     return router
 
 
