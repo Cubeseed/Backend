8d181588cf1496ffac5312900f6aaf8df405556e
Reid-T-W <rediettadesse100@gmail.com>
2023-09-02 14:54:56 +0300

    commodity serializer updated to use hyperlinking

--- cubeseed/commodity/serializers.py
+++ cubeseed/commodity/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import Commodity
 
-
-class CommoditySerializer(serializers.ModelSerializer):
+class CommoditySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Commodity
         fields = "__all__"
