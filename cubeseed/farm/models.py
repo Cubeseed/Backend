e99abfe1fe3fa4a874efa92f4c95d3cd25e0af45
Reid-T-W <rediettadesse100@gmail.com>
2023-09-08 13:26:00 +0300

    Comments and modification added to farm model: a farm must have a commodity assigned

--- cubeseed/farm/models.py
+++ cubeseed/farm/models.py
@@ -12,10 +12,12 @@ class Farm(models.Model):
     business_profile = models.ForeignKey(BusinessProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     size = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Size in Hectares")
-    # The field below will be changed back to OneToOneField
+    # An address can only belong to one farm
+    # and a farm can only have one address
     farm_address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
-    # A farm can exist without a cluster, hence it
-    # is set to null upon deletion of a cluster
+    # A farm is assigned to a cluster after approval
+    # hence it should initally be able to exist without 
+    # a cluster
     cluster = models.ForeignKey(Cluster, on_delete=models.SET_NULL, null=True, related_name="farms", default=None)
-    commodity = models.ForeignKey(Commodity, on_delete=models.SET_NULL, null=True, related_name="farms")
+    commodity = models.ForeignKey(Commodity, on_delete=models.CASCADE, related_name="farms")
 
