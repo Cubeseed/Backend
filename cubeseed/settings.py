37ed9cb2a31fb14291970406703f6b6f02e78149
Sebastian Garcia Helguera <sebastiangarciahelguera@gmail.com>
2023-10-22 16:26:40 -0700

    Add 127.0.0.1 to allowed hosts in development

--- cubeseed/settings.py
+++ cubeseed/settings.py
@@ -18,7 +18,7 @@ from .settings_prod import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS.append("drf_yasg")
 
