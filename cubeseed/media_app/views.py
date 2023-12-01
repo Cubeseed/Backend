25b771bd8fdabad6d92438369f34799a6f18e1d5
Reid-T-W <rediettadesse100@gmail.com>
2023-12-02 00:46:03 +0300

    modification: anyone with the chat multimedia file link can be able to view it

--- cubeseed/media_app/views.py
+++ cubeseed/media_app/views.py
@@ -4,8 +4,5 @@ from django.http import HttpResponse, Http404
 from django.views.static import serve
 
 def serve_media(request, path):
-    # Example: Allow only authenticated users to access media files
-    if not request.user.is_authenticated:
-        raise Http404
     # Delegate to Django's built-in serve view for serving media files
     return serve(request, path, document_root=settings.MEDIA_ROOT)
