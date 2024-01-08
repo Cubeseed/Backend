72546fdc936f097036d9be2ede7b330381a7ddfd
Reid-T-W <rediettadesse100@gmail.com>
2024-01-08 18:24:36 +0300

    settings update: additional test database added in settings.py file

--- cubeseed/settings.py
+++ cubeseed/settings.py
@@ -58,6 +58,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "test_db.sqlite3",
+        },
     }
 }
 
