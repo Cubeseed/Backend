eb5140c4727fa2efccd3f3871a9d1e7534de1e2b
Reid-T-W <rediettadesse100@gmail.com>
2024-01-11 16:16:45 +0300

    documentation: docstring add to middleware of room app

--- cubeseed/room/middleware.py
+++ cubeseed/room/middleware.py
@@ -1,6 +1,4 @@
-"""
-Web socket middleware for token authentication
-"""
+"""Web socket middleware for token authentication"""
 
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
@@ -20,6 +18,19 @@ User = get_user_model()
 
 @database_sync_to_async
 def get_user(validated_token):
+    """
+    Retrieves a user instance based on the user ID in the validated token. 
+    If the user does not exist, returns an AnonymousUser instance.
+
+    Parameters:
+    validated_token: Dict
+        A dictionary containing a validated token. 
+        The token should include a "user_id" key.
+
+    Returns:
+        User: A user instance if the user exists. Otherwise, an AnonymousUser 
+        instance.
+    """
     try:
         user = get_user_model().objects.get(id=validated_token["user_id"])
         return user
@@ -30,10 +41,17 @@ def get_user(validated_token):
 
 
 class JwtAuthMiddleware(BaseMiddleware):
+    """
+    Middleware for JWT Authentication. It validates the JWT token from the query string 
+    and attaches the user to the scope if the token is valid.
+    """
     def __init__(self, inner):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
+        """
+        Validate JWT token, and attach user to the scope.
+        """
        # Close old database connections to prevent usage of timed out connections
         close_old_connections()
 
@@ -51,6 +69,8 @@ class JwtAuthMiddleware(BaseMiddleware):
         else:
             #  Then token is valid, decode it
             decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            # Get the user and add it to the scope, so that
+            # we can access it in the consumer
             scope["user"] = await get_user(validated_token=decoded_data)
         return await super().__call__(scope, receive, send)
 
