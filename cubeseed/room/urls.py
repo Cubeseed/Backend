fb660898aceb09da7dd78da697fa4b2d1816e946
Reid-T-W <rediettadesse100@gmail.com>
2024-01-11 19:10:48 +0300

    documentation and cleanup: module docstring added and unnecessary urls removed

--- cubeseed/room/urls.py
+++ cubeseed/room/urls.py
@@ -1,3 +1,4 @@
+"""Room Urls"""
 from django.urls import path
 from . import views
 from .views import MessagesApi, UploadEndpoint, RefreshURLView, CheckURLView
@@ -7,8 +8,8 @@ urlpatterns = [
     path('upload', UploadEndpoint.as_view(), name="uploads"),
     path('refresh-url/', RefreshURLView.as_view(), name="refresh-url"),
     path('check-url/', CheckURLView.as_view(), name="check-url"),
-    path('', views.rooms, name="rooms"),
-    path('<slug:slug>/', views.room, name="room"),
+    # path('', views.rooms, name="rooms"),
+    # path('<slug:slug>/', views.room, name="room"),
     path('<slug:slug>/messages', MessagesApi.as_view(), name="room"),
 ]
 
